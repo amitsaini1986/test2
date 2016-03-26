79a57746dae005993531652345efbc74b1f440df
Apple <apple@Apples-MacBook-Air.local>
2016-03-26 22:18:48 +0530

    my love abc

--- test2/ViewController.m
+++ test2/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"my love");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
